50ff2f323c62dd185af4fb8b4fac39235e257b9d
Mike Chen <chintown@gmail.com>
2014-02-13 18:20:05 +0800

    fix path of parent js script

--- template/footer.php
+++ template/footer.php
@@ -6,7 +6,7 @@
 
       <script src="//<?=toggle_min_script('ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.js')?>" type="text/javascript"></script>
       <script>window.jQuery || document.write('<script src="<?=toggle_min_script('js/vendor/jquery-1.8.2.js')?>" type="text/javascript"><\/script>')</script>
-      <script src="<?=toggle_min_script(WEB_PATH.'/js/vendor/bootstrap.hacked.js')?>" type="text/javascript"></script>
+      <script src="<?=toggle_min_script(PARENT_WEB_PATH.'/js/vendor/bootstrap.hacked.js')?>" type="text/javascript"></script>
       <?php if (SENTRY_API_JS != '') { ?>
         <script src="//d3nslu0hdya83q.cloudfront.net/dist/1.0/raven.min.js"></script>
         <script>Raven.config(SENTRY_API).install();</script>
