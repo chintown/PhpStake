d89fcd0646c0a95a2a5201f24b021901eb4f8fe9
Mike Chen <chintown@gmail.com>
2014-02-17 14:59:54 +0800

    fix inclusion path in stub

--- entry/stub.php
+++ entry/stub.php
@@ -3,7 +3,7 @@
     $path_fix_before_inc = '../';
     require $path_fix_before_inc.'core/main.inc.php';
     require 'template/header.php';
-    require '../core/authentication.php';
+    require 'core/authentication.php';
 ?>
 
 <div id="___STUB___">
