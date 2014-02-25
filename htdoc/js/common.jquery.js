00cc934ad3d149ca50847da3a27096e3d08224f6
Mike Chen <chintown@gmail.com>
2014-02-25 17:58:23 +0800

    move common method from common.project to common.jquery

--- htdoc/js/common.jquery.js
+++ htdoc/js/common.jquery.js
@@ -109,6 +109,38 @@ function toggleRadio(_this) {
     $this.prop('checked', true);
 }
 
+// -----------------------------------------------------------------------------
+
+function ajaxMsgInfo(msg, seconds) {
+    ajaxMsg(msg, 'alert-info', seconds);
+}
+function ajaxMsgSuccess(msg, seconds) {
+    ajaxMsg(msg, 'alert-success', seconds);
+}
+function ajaxMsgWarning(msg, seconds) {
+    ajaxMsg(msg, '', seconds);
+}
+function ajaxMsgError(msg, seconds) {
+    ajaxMsg(msg, 'alert-error', seconds);
+}
+function ajaxMsg(msg, type, seconds) {
+    seconds = parseInt(seconds, 10) || 3;
+    seconds *= 1000;
+
+    var $tar = $('.ajax-msg');
+    $tar.removeClass('alert-success')
+        .removeClass('alert-error')
+        .removeClass('alert-info')
+    ;
+    $tar.addClass(type);
+    $tar.text(msg).fadeIn();
+
+    if (seconds > 0) {
+        setTimeout(function() {
+            $tar.fadeOut(1000);
+        }, seconds);
+    }
+}
 
 function initDropHint(targets, className) {
     var $body = $('body');
