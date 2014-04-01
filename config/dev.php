70a360880f339ea263a1777003c1d2798d8321d3
Mike Chen <chintown@gmail.com>
2014-04-01 14:02:29 +0800

    add default cookie expiration seconds

--- config/dev.php
+++ config/dev.php
@@ -7,6 +7,8 @@
     // remote (for linode dev) | local (for Mac dev)
     define('ENV','local');
 
+    define('AUTH_COOKIE_SECONDS', 60*60*24); // 24hr
+
     // error logging
     // 'http://03071e4c4c914bc78b720403e57d1ee4:89efa30eab8b43af86192dded6ee2b6c@sentry.chintown.org/5
     define('SENTRY_API_PHP', '');
