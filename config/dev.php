f0c4a52ac893fe7fe866c8789f00d6c79489754b
Mike Chen <chintown@gmail.com>
2014-04-22 10:53:45 +0800

    fix script embedding. also create GA template

--- config/dev.php
+++ config/dev.php
@@ -9,8 +9,8 @@
 
     define('AUTH_COOKIE_SECONDS', 60*60*24); // 24hr
 
-    // error logging
-    // 'http://03071e4c4c914bc78b720403e57d1ee4:89efa30eab8b43af86192dded6ee2b6c@sentry.chintown.org/5
-    define('SENTRY_API_PHP', '');
-    // 'http://03071e4c4c914bc78b720403e57d1ee4@sentry.chintown.org/5'
-    define('SENTRY_API_JS', '');
+    // error logging. http://sentry.chintown.org/
+    define('SENTRY_API_PHP', null);
+    define('SENTRY_API_JS', null);
+
+    define('GA_CODE', null);
--- template/footer.php
+++ template/footer.php
@@ -26,9 +26,19 @@
 
         <script src="<?=toggle_min_script(PARENT_WEB_PATH.'/js/vendor/bootstrap.hacked.js')?>" type="text/javascript"></script>
 
-      <?php if (SENTRY_API_JS !== 'SENTRY_API_JS') { ?>
+      <?php if (constant('SENTRY_API_JS')) { ?>
         <script src="//d3nslu0hdya83q.cloudfront.net/dist/1.0/raven.min.js"></script>
         <script>Raven.config('<?=SENTRY_API_JS?>').install();</script>
+      <?php } ?>
+
+      <?php if (constant('GA_CODE')) { ?>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+          <?=GA_CODE?>
+        </script>
       <?php } ?>
 
       <? include("template/common.project.footer.php"); ?>
